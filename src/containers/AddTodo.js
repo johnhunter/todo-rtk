6dca23d67a774339ef232f10ad619c516980f990
John Hunter <john@johnhunter.info>
2021-01-17 18:19:18 +0000

    Update components to use todosSlice actions

--- src/containers/AddTodo.js
+++ src/containers/AddTodo.js
@@ -1,27 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { connect } from 'react-redux'
-import { addTodo } from '../actions'
+import { addTodo } from 'modules/todos/todosSlice'
 
-const AddTodo = ({ dispatch }) => {
-  let input
+const AddTodo = ({ addTodo }) => {
+  const [todoText, setTodoText] = useState('')
+
+  const onChange = e => setTodoText(e.target.value)
 
   return (
     <div>
       <form
         onSubmit={e => {
           e.preventDefault()
-          if (!input.value.trim()) {
+          if (!todoText.trim()) {
             return
           }
-          dispatch(addTodo(input.value))
-          input.value = ''
+          addTodo(todoText)
+          setTodoText('')
         }}
       >
-        <input ref={node => (input = node)} />
+        <input value={todoText} onChange={onChange} />
         <button type="submit">Add Todo</button>
       </form>
     </div>
   )
 }
 
-export default connect()(AddTodo)
+const mapDispatchToProps = { addTodo };
+
+export default connect(null, mapDispatchToProps)(AddTodo)
