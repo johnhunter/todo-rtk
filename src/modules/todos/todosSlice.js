89ef8d56579eeb65b2eb2cd7c94b2cfcb43186b3
John Hunter <john@johnhunter.info>
2021-01-17 18:06:17 +0000

    Use a prepare callback with an actionCreator

--- src/modules/todos/todosSlice.js
+++ src/modules/todos/todosSlice.js
@@ -1,13 +1,20 @@
 import { createSlice } from '@reduxjs/toolkit'
 
+let nextTodoId = 0
+
 const todosSlice = createSlice({
   name: 'todos',
   initialState: [],
   reducers: {
     // immutable state using https://immerjs.github.io/immer/docs/produce
-    addTodo(state, action) {
-      const { id, text } = action.payload
-      state.push({ id, text, completed: false })
+    addTodo: {
+      reducer(state, action) {
+        const { id, text } = action.payload
+        state.push({ id, text, completed: false })
+      },
+      prepare(text) {
+        return { payload: { text, id: nextTodoId++ } }
+      }
     },
     toggleTodo(state, action) {
       const todo = state.find(todo => todo.id === action.payload)
